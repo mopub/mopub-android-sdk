7ef8ad5a858b4ed2fb9717643c14ee08ecf1ca84
Aleksandr Kolobanov <shadgm05@gmail.com>
2015-10-29 12:03:58 +0300

    use events in mopub native ad

--- mopub-sdk/src/main/java/com/mopub/nativeads/MoPubCustomEventNative.java
+++ mopub-sdk/src/main/java/com/mopub/nativeads/MoPubCustomEventNative.java
@@ -3,9 +3,12 @@ package com.mopub.nativeads;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.mopub.common.VisibleForTesting;
 import com.mopub.common.logging.MoPubLog;
+import com.mopub.nativeads.events.NativeAdEventsObserver;
+import com.mopub.nativeads.events.NativeAdType;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -203,6 +206,19 @@ public class MoPubCustomEventNative extends CustomEventNative {
         @Override
         public String getDaaIconClickthroughUrl() {
             return DAA_CLICKTHROUGH_URL;
+        }
+
+        @Override
+        public void handleClick(@Nullable View view) {
+            super.handleClick(view);
+            NativeAdEventsObserver.instance().onAdClicked(NativeAdType.Mopub);
+        }
+
+        @Override
+        public void recordImpression() {
+            super.recordImpression();
+            NativeAdEventsObserver.instance().onAdImpressed(NativeAdType.Mopub);
+
         }
     }
 }
