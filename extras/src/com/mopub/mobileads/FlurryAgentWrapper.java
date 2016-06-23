a5dc0833870019b4510c17d432080134fda43486
ugo <mail@ugonna.co>
2016-06-22 17:08:28 -0700

    Added support for native video ads

--- extras/src/com/mopub/mobileads/FlurryAgentWrapper.java
+++ extras/src/com/mopub/mobileads/FlurryAgentWrapper.java
@@ -2,6 +2,7 @@ package com.mopub.mobileads;
 
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -12,7 +13,7 @@ public final class FlurryAgentWrapper {
     public static final String PARAM_AD_SPACE_NAME = "adSpaceName";
 
     private static final String ORIGIN_IDENTIFIER = "Flurry_Mopub_Android";
-    private static final String ORIGIN_VERSION = "6.3.1";
+    private static final String ORIGIN_VERSION = "6.4.0";
     private static FlurryAgentWrapper sWrapper;
 
     private FlurryAgent.Builder mAgentBuilder;
@@ -33,9 +34,9 @@ public final class FlurryAgentWrapper {
         FlurryAgent.addOrigin(ORIGIN_IDENTIFIER, ORIGIN_VERSION);
     }
 
-    public synchronized void startSession(Context context, String apiKey) {
+    public synchronized void startSession(@NonNull final Context context, final String apiKey) {
         // validate parameters
-        if (context == null || TextUtils.isEmpty(apiKey)) {
+        if (TextUtils.isEmpty(apiKey)) {
             return;
         }
 
@@ -52,13 +53,13 @@ public final class FlurryAgentWrapper {
         }
     }
 
-    public synchronized void endSession(Context context) {
+    public synchronized void endSession(final Context context) {
         // validate parameters
         if (context == null) {
             return;
         }
 
-	if (FlurryAgent.isSessionActive()) {
+        if (FlurryAgent.isSessionActive()) {
             // sessions are automatic on ICS+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
                 return;
